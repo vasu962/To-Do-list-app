ea4d47887dd4f180d45b7927bd7c09f6fb02f96b
Vasu <vasukumar962@gmail.com>
2024-02-07 18:33:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("you must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+listContainer.addEventListener("click", function(event){
+    if(event.target.tagName === "LI"){
+        event.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(event.target.tagName === "SPAN"){
+        event.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
